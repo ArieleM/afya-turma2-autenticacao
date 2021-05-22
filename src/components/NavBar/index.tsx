d8e90e228eb6b82532563d195259ae754435aa0d
Ariele Mendonça <ariele_apm@outlook.com>
2021-05-22 14:59:25 -0300

    Estiliza NavBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,17 +2,19 @@ import LogoAfya from '../../assets/img/logo.png';
 import { Link } from 'react-router-dom';
 
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const NavBar: React.FC = () => {
   return (
-    <nav>
+    <Container>
       <img src={LogoAfya} alt="Logo Afya" />
+      <div className="links-content">
+        <Link to="/">Home</Link>
+        <Link to="/criar-conta">Inscreva-se</Link>
+        <Link to="/login">Logar</Link>
+      </div>
 
-      <Link to="/">Home</Link>
-      <Link to="/criar-conta">Inscreva-se</Link>
-      <Link to="/login">Logar</Link>
-    </nav>
+    </Container>
   );
 }
 
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.nav`
+background:  #F71755;
+display: flex;
+img{
+  height: 150px;
+  width: auto;
+}
+.links-content{
+  width: 75%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  a{
+    text-decoration: none;
+    background: #fff;
+    padding: 12px;
+    border-radius: 12px;
+    color: #F71755;
+    text-transform: uppercase;
+
+    transition:.3s;
+    &:hover{
+      background: #e2e2ee;
+    }
+  }
+}
+`;
