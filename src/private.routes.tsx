896ec140d92702b0cc51ae5e6146ee944c81f534
Ariele Mendonça <ariele_apm@outlook.com>
2021-05-22 19:17:39 -0300

    Refact token

--- src/private.routes.tsx
+++ src/private.routes.tsx
@@ -1,9 +1,29 @@
 import { Route, Redirect } from 'react-router-dom';
+import jwt from 'jsonwebtoken'
 
 
 const PrivateRoutes: any = ({component: Component, path: Path, ...rest}: any) => {
   
   const isLogin: string | null = localStorage.getItem('@tokenAfyaApp');
+
+  const isSectionActive: any = () => {
+    if(isLogin === undefined || null){
+      return false;
+    }else{
+      const onlyToken: any = isLogin?.split(' ')[1];
+      const tokenPayLoad : any = jwt.decode(onlyToken);
+
+      const expSeconds = tokenPayLoad.exp;
+
+      const timeNow = Date.now() / 1000;
+
+      
+      console.log(expSeconds);
+      
+      return  timeNow > expSeconds ? false : true;
+        
+    }
+  }
 
   return (
     <Route {...rest} render={ props => (
